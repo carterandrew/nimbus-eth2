67c2051b778366c26bcd8510655170ce8420ea8b
cheatfate <eugene.kabanov@status.im>
2025-01-08 14:00:20 +0200

    Remove `quit 1` from the code. Fix copyright year.

--- beacon_chain/sync/sync_overseer.nim
+++ beacon_chain/sync/sync_overseer.nim
@@ -1,5 +1,5 @@
 # beacon_chain
-# Copyright (c) 2018-2024 Status Research & Development GmbH
+# Copyright (c) 2018-2025 Status Research & Development GmbH
 # Licensed and distributed under either of
 #   * MIT license (license terms in the root directory or at https://opensource.org/licenses/MIT).
 #   * Apache v2 license (license terms in the root directory or at https://www.apache.org/licenses/LICENSE-2.0).
@@ -426,8 +426,6 @@ proc mainLoop*(
          dag_backfill_slot = dag.backfill.slot,
          untrusted_tail = shortLog(clist.tail),
          untrusted_head = shortLog(clist.head)
-
-  quit 1
 
   if overseer.isWithinWeakSubjectivityPeriod(currentSlot):
     # Starting forward sync manager/monitor.

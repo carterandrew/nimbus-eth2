84fbe1b5bb0f00456d681be25b2f82886ff443ba
cheatfate <eugene.kabanov@status.im>
2025-01-09 04:25:48 +0200

    Attempt to fix defect state issue with new flag `setLastStateRoot`.

--- beacon_chain/consensus_object_pools/block_clearance.nim
+++ beacon_chain/consensus_object_pools/block_clearance.nim
@@ -270,7 +270,8 @@ proc addHeadBlockWithParent*(
   # onto which we can apply the new block
   let clearanceBlock = BlockSlotId.init(parent.bid, signedBlock.message.slot)
   if not updateState(
-      dag, dag.clearanceState, clearanceBlock, true, cache, dag.updateFlags):
+      dag, dag.clearanceState, clearanceBlock, true, cache, dag.updateFlags,
+      Eth2Digest()):
     # We should never end up here - the parent must be a block no older than and
     # rooted in the finalized checkpoint, hence we should always be able to
     # load its corresponding state
@@ -509,15 +510,13 @@ proc addBackfillBlockData*(
         withBlck(parentBlock):
           forkyBlck.message.state_root
       clearanceBlock = BlockSlotId.init(parent.bid, forkyBlck.message.slot)
-      updateFlags1 = dag.updateFlags + {skipLastStateRootCalculation}
+      updateFlags1 = dag.updateFlags + {setLastStateRoot}
 
     if not updateState(dag, dag.clearanceState, clearanceBlock, true, cache,
-                       updateFlags1):
+                       updateFlags1, trustedStateRoot):
       error "Unable to load clearance state for parent block, " &
             "database corrupt?", clearanceBlock = shortLog(clearanceBlock)
       return err(VerifierError.MissingParent)
-
-    dag.clearanceState.setStateRoot(trustedStateRoot)
 
     let proposerVerifyTick = Moment.now()
 
